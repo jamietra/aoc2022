7c5190ed2c87cefa23fc94431a59d7420bd127c3
Joshua Mitra <joshua@altaml.com>
2022-12-11 21:17:41 -0700

    day11 part 2

--- day11.hs
+++ day11.hs
@@ -1,19 +1,17 @@
-{-# OPTIONS_GHC -Wno-overlapping-patterns #-}
-import Text.Parsec (parse, oneOf)
 import Data.List
 
-data Monkey = Monkey { mID :: Integer
-                     , items :: [Integer]
-                     , operation :: Integer -> Integer
-                     , test :: Integer -> Bool
-                     , trueDest :: Integer
-                     , falseDest :: Integer 
+data Monkey = Monkey { mID :: Int
+                     , items :: [Int]
+                     , operation :: Int -> Int
+                     , test :: Int -> Bool
+                     , trueDest :: Int
+                     , falseDest :: Int 
                      , inspectionCount :: Int}
 
-postInspection :: Integer -> Integer
+postInspection :: Int -> Int
 postInspection = floor . (*) (1/3) . fromIntegral
 
-divisibleTest :: Integer -> Integer -> Bool
+divisibleTest :: Int -> Int -> Bool
 divisibleTest t x = x `mod` t == 0
 
 split :: Eq a => a -> [a] -> [[a]]
@@ -23,55 +21,63 @@ split c xs = fst spanned : split c (snd spanned)
     noC = dropWhile (==c) xs
     spanned = span (/=c) noC
 
-readWithComma :: String -> Integer
+readWithComma :: String -> Int
 readWithComma s = read $ delete ',' s
 
-parseOperation :: [String] -> (Integer -> Integer)
+parseOperation :: [String] -> (Int -> Int)
 parseOperation ["old", "*", "old"] = postInspection . flip (^) 2
-parseOperation [_, "*", s]         = postInspection . (*) (read s :: Integer)
-parseOperation [_, "+", s]         = postInspection . (+) (read s :: Integer)
+parseOperation [_, "*", s]         = postInspection . (*) (read s :: Int)
+parseOperation [_, "+", s]         = postInspection . (+) (read s :: Int)
 parseOperation s = error $ show s
 
-parseOperation2 :: [String] -> (Integer -> Integer)
-parseOperation2 ["old", "*", "old"] = flip (^) 2
-parseOperation2 [_, "*", s]         = (*) (read s :: Integer)
-parseOperation2 [_, "+", s]         = (+) (read s :: Integer)
-parseOperation2 s = error $ show s
+parseOperation2 :: Int -> [String] -> (Int -> Int)
+parseOperation2 n ["old", "*", "old"] = flip mod n . flip (^) 2
+parseOperation2 n [_, "*", s]         = flip mod n . (*) (read s :: Int) 
+parseOperation2 n [_, "+", s]         = flip mod n . (+) (read s :: Int)
+parseOperation2 n s = error $ show s
 
-parseMonkey :: ([String] -> (Integer -> Integer)) -> [String] -> Monkey
+parseMonkey :: ([String] -> (Int -> Int)) -> [String] -> Monkey
 parseMonkey p s = Monkey 
-    { mID = read (head $ split ':' $ last $ words $ head s) :: Integer
+    { mID = read (head $ split ':' $ last $ words $ head s) :: Int
     , items = map readWithComma $ words $ last (split ':' (s !! 1))
     , operation = p (last $ split "=" $ words $ last $ split ':' (s !! 2))
-    , test = divisibleTest (read $ last $ words (s !! 3) :: Integer)
-    , trueDest = read (last $ words (s !! 4)) :: Integer
-    , falseDest = read (last $ words (s !! 5)) :: Integer 
+    , test = divisibleTest (read $ last $ words (s !! 3) :: Int)
+    , trueDest = read (last $ words (s !! 4)) :: Int
+    , falseDest = read (last $ words (s !! 5)) :: Int 
     , inspectionCount = 0}
+
+numToDivBy :: [String] -> Int
+numToDivBy s = read $ last $ words (s !! 3) :: Int
+
+numToModulo :: String -> Int
+numToModulo = product . map numToDivBy . split "" . lines
 
 parseInput1 :: String -> [Monkey]
 parseInput1 = map (parseMonkey parseOperation) . split "" . lines
 
 parseInput2 :: String -> [Monkey]
-parseInput2 = map (parseMonkey parseOperation2) . split "" . lines
+parseInput2 s = map (parseMonkey (parseOperation2 n)) $ split "" $ lines s
+    where
+        n = numToModulo s
 
-processItem :: Monkey -> Integer -> (Integer, Integer)
+processItem :: Monkey -> Int -> (Int, Int)
 processItem m x = (dest, newItem)
     where    
         newItem = operation m x
         dest = if test m newItem then trueDest m else falseDest m 
 
-processMonkey :: Monkey -> (Monkey, [(Integer, Integer)])
+processMonkey :: Monkey -> (Monkey, [(Int, Int)])
 processMonkey m = (m {items = [], inspectionCount = newCount}, map (processItem m) (items m))
     where newCount = inspectionCount m + length (items m)
 
-sendItem :: [Monkey] -> (Integer, Integer) -> [Monkey]
+sendItem :: [Monkey] -> (Int, Int) -> [Monkey]
 sendItem ms i = [addItem m i| m <- ms]
     where 
         addItem monk item 
             | fst item == mID monk = monk {items = items monk ++ [snd item]}
             | otherwise = monk
 
-sendItems :: [Monkey] -> [(Integer, Integer)] -> [Monkey]
+sendItems :: [Monkey] -> [(Int, Int)] -> [Monkey]
 sendItems = foldl sendItem
 
 oneStep :: [Monkey] -> Monkey -> [Monkey]
@@ -111,5 +117,5 @@ main :: IO ()
 main = do
     f <- readFile "input/input11.txt"
     print $ solve1 f
-    -- print $ solve2 f
+    print $ solve2 f
 
